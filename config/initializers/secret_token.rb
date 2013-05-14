c3172b7d48d76d29067620a6bbc9dcef6290bedc
Daniel Chen <daniel85.chen@gmail.com>
2013-05-14 15:42:37 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coffeeapp::Application.config.secret_token = '25b2ca1c358e571b1b7fc7993fab817bab0416318797b75f9ebfc6a4105795b144746ba950c6ff278783bf2555bff6539cf371ce45e659ad8ff3c2be9b16fd26'
